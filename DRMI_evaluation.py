943c1c70baf1326291297ec82f4f9fd751a5fd9c
JoonHyuk Seo <yhy258@naver.com>
2023-10-15 23:14:08 +0900

    Update DRMI_evaluation.py

--- DRMI_evaluation.py
+++ DRMI_evaluation.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
         if config.coord_info:
             config.in_channels = 5
             
-    config.loss_types = ["PSNRLoss", "FrequencyDivLoss"]
-    config.loss_weights = [1.0, 0.5]
+    config.loss_types = ["PSNRLoss"]
+    config.loss_weights = [1.0]
 
 #     config.green_skip = True
     config.fourier_adversarial = True
